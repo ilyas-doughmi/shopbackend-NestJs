241dd15f3fa864d892f2e728fc83115dd4a462d6
Ilyas Doughmi <doughmi.projects@gmail.com>
2026-02-20 21:20:41 +0000

    feat: update findAll method to accept pagination parameters

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
@@ -12,9 +12,10 @@ export class ProductsController {
     return this.productsService.create(createProductDto);
   }
 
-  @Get()
-  findAll() {
-    return this.productsService.findAll();
+  @Get('')
+  findAll(@Query('limit') limit:number, @Query('page') page:number) 
+  {
+    return this.productsService.findAll(limit,page);
   }
 
   @Get(':id')
