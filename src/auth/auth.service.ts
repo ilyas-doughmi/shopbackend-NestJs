4e7f8c70c9c9f2ec06ea1eb17406ce860c89c672
Ilyas Doughmi <doughmi.projects@gmail.com>
2026-02-20 22:28:05 +0000

    feat: implement signIn method in AuthService for user authentication

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
-
+import { loginAuthDto } from './dto/login-auth.dto';
+import * as bycrypt from 'bcrypt';
 
 @Injectable()
+
 export class AuthService {
     constructor(private readonly usersService: UsersService){}
-    
+
+    async signIn(loginAuthDto : loginAuthDto){
+        const user = await this.usersService.findOneByEmail(loginAuthDto.email);
+        if(!user){
+            throw new UnauthorizedException('Invalid Credentials');
+        }
+        const isMatch = await bycrypt.compare(loginAuthDto.password,user.password);
+        
+        if(!isMatch){
+            throw new UnauthorizedException('Invalid Credentials');
+        }
+        
+        const {password,...result} = user;
+
+        return result;
+    }
 }
