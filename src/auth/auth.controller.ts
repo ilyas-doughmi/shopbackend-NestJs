cddc6101fb0d405966035240a34f4385d82d82fe
Ilyas Doughmi <doughmi.projects@gmail.com>
2026-02-20 22:28:10 +0000

    feat: implement signIn endpoint in AuthController for user authentication

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { loginAuthDto } from './dto/login-auth.dto';
+import { AuthService } from './auth.service';
+
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private readonly AuthService: AuthService) {}
+    @Post('/login')
+    signIn(@Body() loginAuthDto:loginAuthDto){
+        return this.AuthService.signIn(loginAuthDto);
+    }
+
+
+
+}
