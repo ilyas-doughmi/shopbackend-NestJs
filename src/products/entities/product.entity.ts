448a8fb6b751553d105f13520e83d1e6c1bc8121
Ilyas Doughmi <doughmi.projects@gmail.com>
2026-02-20 14:50:29 +0000

    feat: define Product entity with properties and TypeORM decorators

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,1 +1,20 @@
-export class Product {}
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+@Entity()
+export class Product {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column()
+    name: string;
+
+    @Column()
+    description: string;
+
+    @Column()
+    price: number;
+
+    @Column()
+    quantity: number;
+
+}
