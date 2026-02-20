df0529b01d088eacc7d3ac9a395e656105475888
Ilyas Doughmi <doughmi.projects@gmail.com>
2026-02-20 21:01:02 +0000

    feat: update CreateProductDto to ensure description is optional and properly validated

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,7 +1,21 @@
-export class CreateProductDto {
-    name: string;
-    price: number;
-    quantity: number;
-    description: string;
+import { IsNotEmpty, IsNumber, IsOptional, IsPositive, IsString } from "class-validator";
 
+export class CreateProductDto {
+    @IsString()
+    @IsNotEmpty()
+    name: string;
+
+    @IsNumber()
+    @IsPositive()
+    @IsNotEmpty()       
+    price: number;
+
+    @IsNumber()
+    @IsPositive()
+    @IsNotEmpty()       
+    quantity: number;
+
+    @IsString()
+    @IsOptional()     
+    description: string;
 }
