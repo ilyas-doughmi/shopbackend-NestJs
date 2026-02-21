104cda00cb39093095a63e670a36e354453e86e8
Ilyas Doughmi <doughmi.projects@gmail.com>
2026-02-21 14:29:32 +0000

    feat: enhance product controller with role-based access control

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -3,34 +3,45 @@ import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { AuthGuard } from 'src/auth/auth/auth.guard';
+import { RequireRoles } from 'src/auth/decorators/roles.decorators';
+import { Roles } from 'src/users/entities/user.entity';
+import { RolesGuard } from 'src/auth/roles/roles.guard';
+
 
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Post()
-  @UseGuards(AuthGuard)
+  @UseGuards(AuthGuard,RolesGuard)
+  @RequireRoles(Roles.Admin)
   create(@Body() createProductDto: CreateProductDto) {
     return this.productsService.create(createProductDto);
   }
 
   @Get('')
+  @UseGuards(AuthGuard)
   findAll(@Query('limit') limit:number, @Query('page') page:number) 
   {
     return this.productsService.findAll(limit,page);
   }
 
   @Get(':id')
+  @UseGuards(AuthGuard)
   findOne(@Param('id') id: string) {
     return this.productsService.findOne(id);
   }
 
   @Patch(':id')
+  @UseGuards(AuthGuard)
+  @RequireRoles(Roles.Admin)
   update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
     return this.productsService.update(id, updateProductDto);
   }
 
   @Delete(':id')
+  @UseGuards(AuthGuard)
+  @RequireRoles(Roles.Admin)
   remove(@Param('id') id: string) {
     return this.productsService.remove(id);
   }
